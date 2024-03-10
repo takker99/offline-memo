eda6b4dea73ead0269c5ea643f0af18677547121
takker99 <37929109+takker99@users.noreply.github.com>
2024-03-10 19:13:47 +0900

    feat:

--- index.js
+++ index.js
@@ -4,101 +4,106 @@
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 
-
+navigator.serviceWorker.register(
+  "serviceworker.js",
+  { scope: "./" },
+).catch((error) => {
+  console.error("Failed to register service worker:", error);
+});
 
 /** @param {string} content
  * @return {string}
-*/
+ */
 const getTitle = (content) => content.split("\n")[0] || "Untitled";
 
 /** @param {string} title
  * @return {void}
-*/
+ */
 const updateTitle = (title) => {
-    document.title = title;
-    const url = new URL(location.href);
-    if (url.search !== title) {
-        url.search = title
-        history.pushState(null, "", url);
-    }
-}
+  document.title = title;
+  const url = new URL(location.href);
+  if (url.hash !== title) {
+    url.search = title;
+    history.pushState(null, "", url);
+  }
+};
 
 /** @param {string} content
- *@param {string} prevTitle
+ * @param {string} prevTitle
  * @return {void}
-*/
+ */
 const emitChange = (content, prevTitle) => {
-    localStorage.removeItem(prevTitle);
-    const title = makeUniqueTitle(getTitle(content));
-    localStorage.setItem(title, content);
-    updateTitle(title);
-}
-
+  localStorage.removeItem(prevTitle);
+  const title = makeUniqueTitle(getTitle(content));
+  localStorage.setItem(title, content);
+  updateTitle(title);
+};
 
 const textArea = document.getElementById("editor");
 let prevTitle = decodeURIComponent(location.search.slice(1) || "Untitled");
 if (textArea instanceof HTMLTextAreaElement) {
-    textArea.value = localStorage.getItem(prevTitle) ?? prevTitle;
-    updateTitle(prevTitle);
+  textArea.value = localStorage.getItem(prevTitle) ?? prevTitle;
+  updateTitle(prevTitle);
 
-    /** @param {string} title
-     * @return {void}
-    */
-    const load = (title) => {
-        const content = localStorage.getItem(title) ?? title;
-        textArea.value = content;
-        emitChange(content, title);
-    }
+  /** @param {string} title
+   * @return {void}
+   */
+  const load = (title) => {
+    const content = localStorage.getItem(title) ?? title;
+    textArea.value = content;
+    emitChange(content, title);
+  };
 
-    textArea.addEventListener("input", () => {
-        emitChange(textArea.value, prevTitle);
-        prevTitle = getTitle(textArea.value);
+  textArea.addEventListener("input", () => {
+    emitChange(textArea.value, prevTitle);
+    prevTitle = getTitle(textArea.value);
+  });
+
+  const pageList = document.getElementById("page-list");
+  if (pageList instanceof HTMLUListElement) {
+    const updateList = () => {
+      pageList.textContent = "";
+      const titles = [...Array(localStorage.length).keys()].flatMap((i) => {
+        const title = localStorage.key(i);
+        return title === null ? [] : [title];
+      }).sort();
+
+      for (const title of titles) {
+        const li = document.createElement("li");
+        li.textContent = title;
+        li.addEventListener("click", () => {
+          load(title);
+          prevTitle = title;
+          updateList();
+          globalThis.scrollTo(0, 0);
+        });
+        pageList.append(li);
+      }
+    };
+    updateList();
+    globalThis.addEventListener("storage", () => {
+      updateList();
     });
 
-    const pageList = document.getElementById("page-list");
-    if (pageList instanceof HTMLUListElement) {
-        const updateList = () => {
-            pageList.textContent = "";
-            const titles = [...Array(localStorage.length).keys()].flatMap((i) => { const title = localStorage.key(i); return title === null ? [] : [title] }).sort();
-
-            for (const title of titles) {
-                const li = document.createElement("li");
-                li.textContent = title;
-                li.addEventListener("click", () => {
-                    load(title);
-                    prevTitle = title;
-                    updateList();
-                    globalThis.scrollTo(0, 0);
-                });
-                pageList.append(li);
-            }
-        }
+    const newButton = document.getElementById("new");
+    if (newButton instanceof HTMLButtonElement) {
+      newButton.addEventListener("click", () => {
+        prevTitle = makeUniqueTitle("Untitled");
+        load(prevTitle);
         updateList();
-        globalThis.addEventListener("storage", () => {
-            updateList();
-        })
-
-        const newButton = document.getElementById("new");
-        if (newButton instanceof HTMLButtonElement) {
-            newButton.addEventListener("click", () => {
-                prevTitle = makeUniqueTitle("Untitled")
-                load(prevTitle);
-                updateList();
-            });
-        }
+      });
     }
+  }
 }
-
-
 
 /** @param {string} title
  * @return {string}
  */
 const makeUniqueTitle = (title) => {
-    let count = 1;
-    let newTitle = title;
-    while (localStorage.getItem(newTitle) !== null) {
-        newTitle = `${title}_${count++}`;
-    }
-    return newTitle;
-}
+  let count = 1;
+  let newTitle = title;
+  while (localStorage.getItem(newTitle) !== null) {
+    newTitle = `${title}_${count++}`;
+  }
+  return newTitle;
+};
---
+++ serviceworker.js
@@ -0,0 +1,54 @@
+// based on https://github.com/mdn/dom-examples/blob/9edd9627054b78f64cabd005cffca5a0808281c9/service-worker/simple-service-worker/sw.js
+
+const cachePromise = globalThis.caches.open("v1");
+
+globalThis.addEventListener("install", (event) => {
+  event.waitUntil((async () =>
+    (await cachePromise).addAll([
+      "./index.html",
+      "./style.css",
+      "./index.js",
+      "./img/favicon.ico",
+      "./img/android-chrome-36x36.png",
+      "./img/android-chrome-48x48.png",
+      "./img/android-chrome-72x72.png",
+      "./img/android-chrome-96x96.png",
+      "./img/android-chrome-128x128.png",
+      "./img/android-chrome-144x144.png",
+      "./img/android-chrome-152x152.png",
+      "./img/android-chrome-192x192.png",
+      "./img/android-chrome-256x256.png",
+      "./img/android-chrome-384x384.png",
+      "./img/android-chrome-512x512.png",
+    ]))());
+});
+
+globalThis.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await cachePromise;
+
+      const request = event.request;
+
+
+      // try to get the resource from the network
+      try {
+        const res = await fetch(request);
+
+        if (res.ok) {
+          await cache.put(request, res.clone());
+        } else {
+          // try to use a cached response
+          const cached = await cache.match(request);
+          if (cached) return cached;
+        }
+
+        return res;
+      } catch (_) {
+        // try to use a cached response
+        const res = await cache.match(request);
+        if (res) return res;
+      }
+    })(),
+  );
+});
